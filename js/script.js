ec453d1e563be478bde066765f2cb6e580e44a81
Anton <abodyk1516@gmail.com>
2023-10-08 18:34:01 +0300

    finish project

--- js/script.js
+++ js/script.js
@@ -1,67 +1,80 @@
-let inputValueAmount = document.getElementById('amount');
-let replenishmentFormButton = document.querySelector('.replenishment-button');
+const inputValueAmount = document.getElementById('amount');
+const replenishmentFormButton = document.querySelector('.replenishment-button');
+const categoryInput = document.getElementById('category-input');
+const categoryButton = document.querySelector('.category-button');
+const categoryArray = JSON.parse(localStorage.getItem('categoryArray')) || [];
+const openModalButton = document.querySelector('.add-expenses-button');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.modal__close');
+const btnAddSumExpenses = document.querySelector('.btn-sum-expenses');
+const inputAddComment = document.querySelector('.modal__input__comment');
+const inputAddExpenses = document.querySelector('.modal__input__expenses');
+const expensesArray = JSON.parse(localStorage.getItem('expensesArray')) || [];
 
-function upUserBalance(){
-
+function upUserBalance() {
     replenishmentFormButton.addEventListener('click', (e) => {
+        const inputValue = parseFloat(inputValueAmount.value);
 
-        if(isNaN(inputValueAmount.value)){
+        if (isNaN(inputValueAmount.value)) {
             alert('Не правильные данные');
             inputValueAmount.value = '';
-        }else if(inputValueAmount.value === ''){
+        } else if (inputValueAmount.value === '') {
             alert('Введите данные');
-        }else if(inputValueAmount.value < 0){
+        } else if (inputValueAmount.value < 0) {
             alert('Введите значение больше чем 0');
             inputValueAmount.value = '';
-        }else{
+        } else {
             e.preventDefault();
             console.log(inputValueAmount);
-                
-            localStorage.setItem('amount', parseInt(localStorage.getItem('amount')) + parseInt(inputValueAmount.value));
-    
+
+            const currentBalance = parseFloat(localStorage.getItem('amount') || 0);
+
+            localStorage.setItem('amount', (currentBalance + inputValue).toString());
+
             inputValueAmount.value = '';
             location.reload();
         }
     })
 }
 
-
-function userBalance(){
+function displayBalance(){
     const showUserBalance = document.getElementById('general-balance');
-    showUserBalance.innerHTML = localStorage.getItem('amount');
+    showUserBalance.innerHTML = parseFloat(localStorage.getItem('amount') || 0);
 }
-
-const categoryInput = document.getElementById('category-input');
-const categoryButton = document.querySelector('.category-button');
-const categoryArray = JSON.parse(localStorage.getItem('categoryArray')) || [];
-
 
 function addCategoryExpenses(){
+    const categorySelect = document.getElementById('category-select');
+    categorySelect.innerHTML = ''; 
+
+    categoryArray.forEach((category) => {
+        const option = document.createElement('option');
+
+        option.value = category;
+        option.textContent = category;
+        categorySelect.appendChild(option);
+    });
 
     categoryButton.addEventListener('click', () => {
-        
-        if(isNaN(categoryInput.value)){
-            if(categoryArray.includes(categoryInput.value)){
-                alert('Такая категория уже добавлена!');
-                categoryInput.value = '';
-            }else{
-                categoryArray.push(categoryInput.value);
-                localStorage.setItem('categoryArray', JSON.stringify(categoryArray));
-                categoryInput.value = '';
-            }
-        }else if(categoryInput.value === ''){
-            alert('Введите данные');
-        }else{
-            alert('Не правильные данные');
+        const newCategory = categoryInput.value.trim();
+
+        if (newCategory === '') {
+            alert('Введите категорию');
+        } else if (!categoryArray.includes(newCategory)) {
+            categoryArray.push(newCategory);
+            localStorage.setItem('categoryArray', JSON.stringify(categoryArray));
+
+            const option = document.createElement('option');
+            option.value = newCategory;
+            option.textContent = newCategory;
+            categorySelect.appendChild(option);
+
+            categoryInput.value = '';
+        } else {
+            alert('Такая категория уже добавлена!');
             categoryInput.value = '';
         }
-        
-    })
+    });
 }
-
-const openModalButton = document.querySelector('.add-expenses-button'),
-      modal = document.querySelector('.modal'),
-      closeModal = document.querySelector('.modal__close');
 
 function openModal(){
     modal.classList.add('show');
@@ -73,35 +86,31 @@ function removeModal(){
     document.body.style.overflow = '';
 }
 
-
-const btnAddSumExpenses = document.querySelector('.btn-sum-expenses');
-const inputAddComment = document.querySelector('.modal__input__comment');
-const inputAddExpenses = document.querySelector('.modal__input__expenses');
-const expensesArray = JSON.parse(localStorage.getItem('expensesArray')) || [];
-
 function userExpenses(){
-
     btnAddSumExpenses.addEventListener('click', (e) => {
-        
+        const selectedCategory = document.getElementById('category-select').value;
+        const expensesAmount = parseInt(inputAddExpenses.value);
 
-        if(isNaN(inputAddExpenses.value)){
+        if (selectedCategory === '') {
+            alert('Выберите категорию');
+        } else if (isNaN(expensesAmount)) {
             alert('Не правильные данные');
-        }else if(inputAddExpenses.value === '' || inputAddComment.value === ''){
+        } else if (inputAddExpenses.value === '' || inputAddComment.value === '') {
             alert('Введите данные');
-        }else if(typeof(inputAddComment.value) !== 'string'){
-            alert('Значение может быть только строкой');
-        }else if(localStorage.getItem('amount') < parseInt(inputAddExpenses.value)){
+        } else if (typeof (inputAddComment.value) !== 'string') {
+            alert('Значение комментария может быть только строкой');
+        } else if (localStorage.getItem('amount') < expensesAmount) {
             alert('На вашем балансе не достаточно средств!');
             inputAddComment.value = '';
             inputAddExpenses.value = '';
-        }else{
+        } else {
             e.preventDefault();
-    
+
             localStorage.setItem('comment', inputAddComment.value);
-            expensesArray.push(inputAddExpenses.value);
+            expensesArray.push({category: selectedCategory, amount: expensesAmount});
             localStorage.setItem('expensesArray', JSON.stringify(expensesArray));
-            localStorage.setItem('amount', parseInt(localStorage.getItem('amount')) - parseInt(inputAddExpenses.value));
-    
+            localStorage.setItem('amount', (parseFloat(localStorage.getItem('amount')) - expensesAmount).toString());
+
             inputAddComment.value = '';
             inputAddExpenses.value = '';
             removeModal();
@@ -110,6 +119,14 @@ function userExpenses(){
     });
 }
 
+const categories = JSON.parse(localStorage.getItem('categoryArray')) || [];
+const expensesData = JSON.parse(localStorage.getItem('expensesArray')) || [];
+
+const categoryLabels = categories.map((category) => category);
+const expenseAmounts = categories.map((category) => {
+    const categoryExpenses = expensesData.filter((expense) => expense.category === category);
+    return categoryExpenses.reduce((sum, expense) => sum + expense.amount, 0);
+});
 
 const ctx = document.getElementById('myChart');
 
@@ -117,10 +134,10 @@ const ctx = document.getElementById('myChart');
 let chart = new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: JSON.parse(localStorage.getItem('categoryArray')),
+      labels: categoryLabels,
       datasets: [{
         label: 'Сумма затрат',
-        data: JSON.parse(localStorage.getItem('expensesArray')),
+        data: expenseAmounts,
         borderWidth: 1
       }]
     },
@@ -138,7 +155,7 @@ let chart = new Chart(ctx, {
 
 document.addEventListener('DOMContentLoaded', () => {
     upUserBalance();
-    userBalance();
+    displayBalance();
     userExpenses();
     addCategoryExpenses();
 
